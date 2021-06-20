8d464282661d728fe49b9fd4426077ba7e12d7e4
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-06-20 12:11:51 +0600

    Strings
    Some examples of string manipulation.

---
+++ string.py
@@ -0,0 +1,17 @@
+greetings = 'Hello ! How are you?'
+
+spam = 'eggs are not round'
+
+spam1 = 'But I don\'t love eggs'
+spam2 = 'Hasnat\'s look so cool today!'
+
+newline = 'Hasnat \nLooks so cool'
+
+style = """
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+"""
+
+s = 'supercalifragilisticexpialidocious'
+print(s[0:34:2])
