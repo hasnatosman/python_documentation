806d1958b26bdb56b399384d3b2b04d7563e96b5
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-06-23 11:49:13 +0600

    Add files via upload

---
+++ list_1.py
@@ -0,0 +1,22 @@
+fruits = ['orange', 'apple', 'pear', 'mango', 'kiwi', 'apple', 'banana']
+
+fruits.insert(2, 'jackfruit')
+# add new item in an specific index................
+
+fruits.append('mango')
+# add any item in the last of the list.............
+
+fruits.remove('apple')
+# remove any item from the list....................
+
+fruits.pop()
+# pop up the last item from the list................
+
+fruits.sort()
+# sort the list ....................................
+
+fruits.reverse()
+# reverse the list.................................
+
+
+print(fruits)
